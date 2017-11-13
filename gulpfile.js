315ff80b88340d9112470b41abb3b4e6a3b5d275
Guest User <Guest@Epicodus-3D.local>
2017-11-13 09:21:17 -0800

    fix linking issue

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
-var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream'); //vinyl-source-stream is used for putting the browserified source code into a new file
+var gulp = require('gulp');
 
 gulp.task('myTask', function(){
   console.log('hello gulp');
@@ -8,7 +8,7 @@ gulp.task('myTask', function(){
 
 
 gulp.task('jsBrowserify', function() {
-  return browserify({ entries: ['./jspingpong-interface.js'] })
+  return browserify({ entries: ['./js/pingpong-interface.js'] })
     .bundle()
     .pipe(source('app.js'))
     .pipe(gulp.dest('./build/js'));
