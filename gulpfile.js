608d0dda19f76c912c8f7984e3070f7d487282f5
Guest User <Guest@Epicodus-3D.local>
2017-11-13 10:21:33 -0800

    add optimization packages; add build gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,44 @@
 var browserify = require('browserify');
 var source = require('vinyl-source-stream'); //vinyl-source-stream is used for putting the browserified source code into a new file
+var concat = require('gulp-concat'); //create a script by contatinating existing script files into a single file the bowser can read faster
+var uglify = require('gulp-uglify'); // remove all unnecessary characters in JS files to optimize JavaScript execution
+var utilities = require('gulp-util');
+var del = require('del');
 var gulp = require('gulp');
 
 gulp.task('myTask', function(){
   console.log('hello gulp');
 });
 
+gulp.task("minifyScripts", ["jsBrowserify"], function(){
+  return gulp.src("./build/js/app.js")
+  .pipe(uglify())
+  .pipe(gulp.dest("./build/js"));
+});
 
-gulp.task('jsBrowserify', function() {
-  return browserify({ entries: ['./js/pingpong-interface.js'] })
+gulp.task('jsBrowserify', ['concatInterface'], function() {
+  return browserify({ entries: ['./tmp/allConcat.js'] })
     .bundle()
     .pipe(source('app.js'))
     .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('concatInterface', function() {
+  return gulp.src(['./js/*-interface.js'])
+    .pipe(concat('allConcat.js'))
+    .pipe(gulp.dest('./tmp'));
+});
+
+var buildProduction = utilities.env.production;
+
+gulp.task("clean", function(){
+  return del(['build', 'tmp']);
+});
+
+gulp.task("build", ['clean'], function(){
+  if (buildProduction) {
+    gulp.start('minifyScripts');
+  } else {
+    gulp.start('jsBrowserify');
+  }
 });
